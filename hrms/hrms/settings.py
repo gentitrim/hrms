6e6b8a18d388be9e16c85e4705386241e7c16921
petritfisheku <ritifisheku1@gmail.com>
2025-03-01 23:08:52 +0100

    test

--- hrms/hrms/settings.py
+++ hrms/hrms/settings.py
@@ -16,7 +16,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(r"C:\Users\HP\Desktop\Projekte\HRMS\.env")
+load_dotenv(r"C:\Users\ritif\OneDrive\Desktop\bar-restorant\env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
