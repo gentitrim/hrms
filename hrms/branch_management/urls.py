c274a5de00138f7969c2511db7c8577c7c92d6bf
silviafrroku <silviafrroku28@gmail.com>
2025-04-12 10:59:32 +0200

    Create ,Update, Delete  for Categories

--- hrms/branch_management/urls.py
+++ hrms/branch_management/urls.py
@@ -1,3 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'branch_managment'
+
+urlpatterns = [
+    path('categories/', views.CategoryListView.as_view(), name='category-list'),
+    path('categories/create/', views.CategoryCreateView.as_view(), name='category-create'),
+    path('categories/<int:pk>/update/', views.CategoryUpdateView.as_view(), name='category-update'),
+    path('categories/<int:pk>/delete/', views.CategoryDeleteView.as_view(), name='category-delete'),
+]
